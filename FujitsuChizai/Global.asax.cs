690ad342454883e11ab6910f18358cb2d99f3893
4423 <hanamiya4423@gmail.com>
2016-08-16 21:16:14 +0900

    JSONをLowerCamelCaseで返す

--- FujitsuChizai/Global.asax.cs
+++ FujitsuChizai/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,10 @@ namespace FujitsuChizai
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // JSONをLowerCamelCaseで返す
+            var config = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
