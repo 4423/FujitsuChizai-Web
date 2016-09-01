d999d6dc040104e874d2d43bf03835b03f15cf50
4423 <hanamiya4423@gmail.com>
2016-09-01 22:43:33 +0900

    Place間でも経路生成できるようにアルゴリズムを修正

--- FujitsuChizai/Models/RouteFinding.cs
+++ FujitsuChizai/Models/RouteFinding.cs
@@ -16,6 +16,7 @@ namespace FujitsuChizai.Models
         private static int[,] Dist;
         private static int[,] Pred;
         private static readonly int INF = int.MaxValue / 2;
+        private static readonly int PLACE_COST = 10000; // Placeを経由しない程度の重み
 
         public bool IsInitialized => Dist != null;
 
@@ -46,12 +47,12 @@ namespace FujitsuChizai.Models
         public void InitializeShortestPathTree()
         {
             // ノードの数だけ配列を確保
-            var v = db.PlaceMarks.OrderByDescending(x => x.Id).First().Id; // .Last()が使えない
-            Dist = new int[v+10, v+10];
-            Pred = new int[v+10, v+10];
-            for (int i = 0; i < v; i++)
+            var v = 1 + db.PlaceMarks.OrderByDescending(x => x.Id).First().Id; // .Last()が使えない
+            Dist = new int[v, v];
+            Pred = new int[v, v];
+            for (int i = 1; i < v; i++)
             {
-                for (int j = 0; j < v + 10; j++)
+                for (int j = 1; j < v; j++)
                 {
                     Dist[i, j] = INF;
                     Pred[i, j] = -1;
@@ -59,24 +60,24 @@ namespace FujitsuChizai.Models
                 Dist[i, i] = 0; // 自身に対して重み0
             }
 
-            // Placeを除く重み付き無向グラフ
-            foreach (var e in db.Edges.Where(x => x.PlaceMark1.Type != PlaceMarkType.Place && x.PlaceMark2.Type != PlaceMarkType.Place))
+            var edges = db.Edges.Select(x =>
+                new
+                {
+                    Id1 = x.PlaceMarkId1,
+                    Id2 = x.PlaceMarkId2,
+                    Cost = x.Cost,
+                    IsPlaceEdge = x.PlaceMark1.Type == PlaceMarkType.Place || x.PlaceMark2.Type == PlaceMarkType.Place
+                });
+            // 重み付き無向グラフ構築
+            foreach (var e in edges)
             {
-                Dist[e.PlaceMarkId1, e.PlaceMarkId2] = Dist[e.PlaceMarkId2, e.PlaceMarkId1] = e.Cost;
-                Pred[e.PlaceMarkId1, e.PlaceMarkId2] = e.PlaceMarkId1;
-                Pred[e.PlaceMarkId2, e.PlaceMarkId1] = e.PlaceMarkId2;
+                Dist[e.Id1, e.Id2] = Dist[e.Id2, e.Id1] = e.IsPlaceEdge ? PLACE_COST : e.Cost; // Placeを含む辺ならPLACE_COSTが重み
+                Pred[e.Id1, e.Id2] = e.Id1;
+                Pred[e.Id2, e.Id1] = e.Id2;
             }
 
             // 計算
             WarshallFloyd(v);
-
-            // グラフにPlaceを追加
-            foreach (var e in db.Edges.Where(x => x.PlaceMark1.Type == PlaceMarkType.Place || x.PlaceMark2.Type == PlaceMarkType.Place))
-            {
-                Dist[e.PlaceMarkId1, e.PlaceMarkId2] = Dist[e.PlaceMarkId2, e.PlaceMarkId1] = e.Cost;
-                Pred[e.PlaceMarkId1, e.PlaceMarkId2] = e.PlaceMarkId1;
-                Pred[e.PlaceMarkId2, e.PlaceMarkId1] = e.PlaceMarkId2;
-            }
         }
 
         public IEnumerable<StepViewModel> GenerateSteps(PlaceMark start, PlaceMark goal)
