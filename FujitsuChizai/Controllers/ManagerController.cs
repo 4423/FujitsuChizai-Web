64872d8e6d504201d7bfe2c65084805cf1cbce4e
4423 <hanamiya4423@gmail.com>
2016-09-01 18:43:41 +0900

    管理ページの作成

---
+++ FujitsuChizai/Controllers/ManagerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FujitsuChizai.Models.Entities;
+
+namespace FujitsuChizai.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private ModelContext db = new ModelContext();
+
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View(db.Maps.ToList());
+        }
+
+        // GET: Manager/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Map map = db.Maps.Find(id);
+            if (map == null)
+            {
+                return HttpNotFound();
+            }
+            return View(map);
+        }
+
+        // GET: Manager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manager/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Floor,Description,MapImageFilePath,Width,Height")] Map map)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Maps.Add(map);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(map);
+        }
+
+        // GET: Manager/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Map map = db.Maps.Find(id);
+            if (map == null)
+            {
+                return HttpNotFound();
+            }
+            return View(map);
+        }
+
+        // POST: Manager/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Floor,Description,MapImageFilePath,Width,Height")] Map map)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(map).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(map);
+        }
+
+        // GET: Manager/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Map map = db.Maps.Find(id);
+            if (map == null)
+            {
+                return HttpNotFound();
+            }
+            return View(map);
+        }
+
+        // POST: Manager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Map map = db.Maps.Find(id);
+            db.Maps.Remove(map);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
