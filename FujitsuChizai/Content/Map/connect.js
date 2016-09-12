6f29f6a4469533bdd259b8f623c053525f1f7ade
4423 <hanamiya4423@gmail.com>
2016-09-12 17:42:51 +0900

    データ登録ページのリソースをファイル分割

---
+++ FujitsuChizai/Content/Map/connect.js
@@ -0,0 +1,21 @@
+﻿function registerPath() {
+    $('path').each(function (i, e) {
+        post($(e).attr('id1'), $(e).attr('id2'), $(e).attr('cost'));
+    });
+}
+
+function post(id1, id2, cost) {
+    jQuery.ajax({
+        url: '@Request.Url.AbsoluteUri.Replace(Request.RawUrl, "")/api/edges',
+        type: 'POST',
+        dataType: 'json',
+        data: { placeMarkId1: id1, placeMarkId2: id2, cost: cost },
+        timeout: 10000,
+        success: function (data) {
+            alert("ok");
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("error");
+        }
+    });
+}
---
+++ FujitsuChizai/Content/Map/zoom.js
@@ -0,0 +1,14 @@
+﻿var zoomRatio = 0.5;
+
+function setZoomRatio() {
+    $('#canvas').css('transform', 'scale(' + zoomRatio + ')');
+}
+
+function zoomOut() {
+    zoomRatio -= 0.1;
+    setZoomRatio();
+}
+function zoomIn() {
+    zoomRatio += 0.1;
+    setZoomRatio();
+}
