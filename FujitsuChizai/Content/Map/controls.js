11bd31dd50af2b906eff8a9cfb9eef718147cc63
4423 <hanamiya4423@gmail.com>
2016-09-12 20:40:17 +0900

    一つだけ開かれるようにcontrolsを修正

--- FujitsuChizai/Content/Map/controls.js
+++ FujitsuChizai/Content/Map/controls.js
@@ -1,6 +1,25 @@
-﻿$(function () {
+﻿var mode = null;
+
+$(function () {
     $(".controls a").click(function () {
-        var mode = $(this).attr("id");
-        $(".slide_box#"+ mode).slideToggle("fast");
+        // 閉じる
+        if (mode != null) {
+            $(".slide_box#" + mode).slideToggle("fast");
+            // 閉じて別のを開く
+            currMode = $(this).attr("id");
+            if (mode != currMode) {
+                mode = currMode;
+                $(".slide_box#" + mode).slideToggle("fast");
+            }
+            // 閉じるだけ
+            else {
+                mode = null;
+            }
+        }
+        // 開ける
+        else {
+            mode = $(this).attr("id");
+            $(".slide_box#" + mode).slideToggle("fast");
+        }
     });
 });
