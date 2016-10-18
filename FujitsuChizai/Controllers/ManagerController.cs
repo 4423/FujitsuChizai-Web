eb1b30bd2aa5a56d15724016bc8b2061debd8e48
4423 <hanamiya4423@gmail.com>
2016-10-18 11:42:39 +0900

    MapViewModelのコード移動

--- FujitsuChizai/Controllers/ManagerController.cs
+++ FujitsuChizai/Controllers/ManagerController.cs
@@ -13,20 +13,7 @@ using System.Drawing;
 using FujitsuChizai.Models;
 
 namespace FujitsuChizai.Controllers
-{
-    public class MapViewModel
-    {
-        public List<PlaceMark> PlaceMarks { get; set; }
-        public Map Map { get; set; }
-        public List<PathData> Paths { get; set; }
-        public class PathData
-        {
-            public Edge Edge { get; set; }
-            public string D { get; set; }
-            public string StrokeDash { get; set; }
-        }
-    }
-    
+{   
     public class ManagerController : Controller
     {
         private ModelContext db = new ModelContext();
---
+++ FujitsuChizai/Models/MapViewModels.cs
@@ -0,0 +1,21 @@
+﻿using FujitsuChizai.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FujitsuChizai.Models
+{
+    public class MapViewModel
+    {
+        public List<PlaceMark> PlaceMarks { get; set; }
+        public Map Map { get; set; }
+        public List<PathData> Paths { get; set; }
+        public class PathData
+        {
+            public Edge Edge { get; set; }
+            public string D { get; set; }
+            public string StrokeDash { get; set; }
+        }
+    }
+}
