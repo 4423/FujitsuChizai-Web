a4f6a4493c56bd3c4945839c78158f19a4d9a46f
4423 <hanamiya4423@gmail.com>
2016-11-04 20:55:01 +0900

    登録直後に更新・接続・削除ができない不具合の修正

--- FujitsuChizai/Content/Map/placemark.js
+++ FujitsuChizai/Content/Map/placemark.js
@@ -38,7 +38,12 @@
 
     // この placemark をサーバに登録
     register(successCallback, errorCallback) {
-        Http.asyncPost(this.ENDPOINT, this.serialize(), successCallback, errorCallback);
+        var that = this;
+        var callback = function (json) {
+            successCallback();
+            that.id = json.id; // 割り当てられたIDを反映
+        };
+        Http.asyncPost(this.ENDPOINT, this.serialize(), callback, errorCallback);
     }
 
     // この placemark をサーバに更新
